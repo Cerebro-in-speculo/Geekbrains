09385386ea1636e1337994c25a6fe78c16070760
Cerebro-in-speculo <r.ledovskih@gmail.com>
2021-08-28 11:32:44 +0300

    add hw_8 all

--- hw_8/src/ru/geekbrains/homework_8/Main.java
+++ hw_8/src/ru/geekbrains/homework_8/Main.java
@@ -1,8 +1,47 @@
 package ru.geekbrains.homework_8;
 
 public class Main {
+    public interface Discipline {
+        public void running();
+
+        public void jump();
+
+        public int getMax_player_length();
+
+        public int getMax_player_height();
+
+        public void setDo(boolean isDo);
+
+        public boolean getDo();
+    }
+
+    public interface Competitions {
+        public void action(Discipline players);
+    }
 
     public static void main(String[] args) {
-	// write your code here
+
+        Discipline[] players = {
+                new Human(50, 50), new Robot(50, 100),
+                new Cat(100, 100), new Robot(100, 100),
+                new Robot(100, 100), new Human(100, 100)
+        };
+
+        Competitions[] games = {
+                new Track(20), new Wall(100),
+                new Track(100), new Track(100),
+                new Wall(100), new Wall(100)
+        };
+        myCompetitions(players, games);// Старт!!!)
+    }
+
+    public static void myCompetitions(Discipline[] players, Competitions[] games) {
+        for (Discipline player : players) {
+            for (Competitions game : games) {
+                if (player.getDo()) {
+                    game.action(player);
+                }
+            }
+        }
     }
 }
